2d4ed30ee2a38efc4cece460870452d562322a4c
Santiago Salazar Parra <ssantia4567@gmail.com>
2025-09-30 00:23:15 -0500

    clean repository now includes the gitignore file

--- BackendStore/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ BackendStore/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7deaeb490741a70ee00f251a1c34ceaca9de2600")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
